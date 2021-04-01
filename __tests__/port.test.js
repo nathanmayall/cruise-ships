2d0685e4d5919beecdf28984f7d24823d9e055cd
Nathan Mayall <70609396+nathanmayall@users.noreply.github.com>
2021-04-01 18:06:03 +0100

    add mocks etc

--- __tests__/port.test.js
+++ __tests__/port.test.js
@@ -1,17 +1,19 @@
 const { Port } = require("../src/Port");
 
 describe("Port", () => {
-  it("has a name", () => {
-    const port = new Port("Dover");
+  let port;
 
+  beforeEach(() => {
+    port = new Port("Dover");
+  });
+
+  it("has a name", () => {
     expect(port.name).toBe("Dover");
   });
+
   it("can have a ship added", () => {
-    const port = new Port("Dover");
-    const ship = {};
-
+    const ship = jest.fn();
     port.addShip(ship);
-
     expect(port.ships).toContain(ship);
   });
 });
